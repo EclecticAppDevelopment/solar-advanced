13448bec6543147b47abf4b0825c650a7ff974a0
eclectic-matt <eclecticappdevelopment@gmail.com>
2014-05-22 22:19:01 +0100

    Update solAdvanced.java
    Planet classes implemented

--- solAdvanced.java
+++ solAdvanced.java
@@ -25,54 +25,29 @@ import com.badlogic.gdx.graphics.g3d.environment.*;
 
 public class solAdvanced implements ApplicationListener
 {
-	/** Based on...* See: http://blog.xoppa.com/basic-3d-using-libgdx-2 * @author Xoppa */
-	float dT;
+	float dT, camPos;
 
 	public Environment environment;
 	public PerspectiveCamera cam;
 	public CameraInputController camController;
 	public ModelBatch modelBatch;
-	// solModel sol solR, solC,
-	public Model mercModel, venModel, earthModel, marsModel, jupModel;
-	public Model satModel, uraModel, nepModel, pluModel;
-	public ModelInstance mercury, venus, earth, mars, jupiter;    
-    public ModelInstance saturn, uranus, neptune, pluto;
-  	public float solR, merD, merR, venD, venR, earR, earD, marR, marD, jupR, jupD;
-	public Color merC, venC, earC, marC, jupC;
-	public float satR, uraR, nepR, pluR;
-	public float satD, uraD, nepD, pluD;
-	public Color satC, uraC, nepC, pluC;
 	public Color dirLights;
 	public float camFact;
 	public Vector3 touchPoint;
 
 	public class Planet{
-		public float x, y, z, w, h, d;
+		public float x, y, z, r, d;
 		public int div;
 		public Color c;
 		public Model m;
 		public ModelInstance i;
 	}
-	public Planet Sol;
+	public Planet Sol, Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune, Pluto;
 
 
 	@Override
 	public void create() {
 
-		touchPoint = new Vector3();
-		Sol = new Planet();
-//		Sol.c.set(Color.ORANGE.add(Color.YELLOW));
-		Sol.x = 0;
-		Sol.y = 0;
-		Sol.z = 0;
-		Sol.div = 40;
-		Sol.c = new Color(Color.ORANGE.add(Color.YELLOW));
-		Sol.w = 695.5f;
-		Sol.h = Sol.w;
-		Sol.d = Sol.w;
-		solR = Sol.w;
-
-		dT = 0;
 		// Environment for lights
 		environment = new Environment();
 		// Model batch to draw all stars/planets
@@ -80,118 +55,161 @@ public class solAdvanced implements ApplicationListener
 		// Builds each object
 		ModelBuilder modelBuilder = new ModelBuilder();
 
+		dT = 0;
+		camPos = 2000f;
 		dirLights = new Color(Color.WHITE);
 		camFact = 250f;
 		EnviroLights();
 		CameraSet();
-
-		// Sun's diameter is 1 391 000 km
-		//	solR = 139.1f;
-		//	solC = new Color(Color.ORANGE.add(Color.YELLOW));
-
-		// Mercury's radius is 5 000 km
-		merR = 5f;
-		// Mercury is 58 000 000 km
-		merD = solR + 58f;
-		merC = new Color(Color.LIGHT_GRAY);
-		venR = 12f;
-		venD = solR + 108f;
-		venC = new Color(Color.MAGENTA);
-		earR = 13f;
-		earD = solR + 150f;
-		earC = new Color(Color.CYAN);
-		marR = 7f;
-		marD = solR + 228f;
-		marC = new Color(Color.RED);
-		jupR = 143f;
-		jupD = solR + 778f;
-		jupC = new Color(Color.DARK_GRAY);
-		satR = 120f;
-		satD = solR + 1080f;
-		satC = new Color(Color.WHITE);
-		uraR = 40f;
-		uraD = solR + 1500f;
-		uraC = new Color(Color.BLUE);
-		nepR = 70f;
-		nepD = solR + 2280f;
-		nepC = new Color(Color.GREEN);
-		pluR = 3f;
-		pluD = solR + 7780f;
-		pluC = new Color(Color.GRAY);
-
-		//*** Keep together in create
-//		solModel = modelBuilder.createSphere(solR, solR, solR, 40, 40,
-//											 new Material(ColorAttribute.createDiffuse(solC)),
-//											 Usage.Position | Usage.Normal);
-//		sol = new ModelInstance(solModel);
-
-		Sol.m = modelBuilder.createSphere(Sol.w, Sol.h, Sol.d, Sol.div, Sol.div,
+    	touchPoint = new Vector3();
+		//
+		Sol = new Planet();
+		Sol.d = 0;
+		Sol.x = 0;
+		Sol.y = 0;
+		Sol.z = 0;
+		Sol.div = 80;
+		Sol.c = new Color(Color.ORANGE.add(Color.YELLOW));
+		Sol.r = 1391.1f;
+		Sol.m = modelBuilder.createSphere(Sol.r, Sol.r, Sol.r, Sol.div, Sol.div,
 										  new Material(ColorAttribute.createDiffuse(Sol.c)),
 										  Usage.Position | Usage.Normal);
 		Sol.i = new ModelInstance(Sol.m);
-		//	Sol.i.transform.translate(Sol.x, Sol.y, Sol.z);
+		Sol.i.transform.translate(Sol.x, Sol.y, Sol.z);
 		//
-		mercModel = modelBuilder.createSphere(merR, merR, merR, 40, 40, 
-											  new Material(ColorAttribute.createDiffuse(merC)),
+		Mercury = new Planet();
+		Mercury.d = Sol.r + 580f;
+		Mercury.r = 5f;
+		Mercury.c = new Color(Color.LIGHT_GRAY);
+		Mercury.x = Mercury.d;
+		Mercury.y = 0;
+		Mercury.z = 0;
+		Mercury.div = 40;
+		Mercury.m = modelBuilder.createSphere(Mercury.r, Mercury.r, Mercury.r, Mercury.div, Mercury.div,
+											  new Material(ColorAttribute.createDiffuse(Mercury.c)),
 											  Usage.Position | Usage.Normal);
-		mercury = new ModelInstance(mercModel);
-		mercury.transform.translate(merD, 0, merD);
+		Mercury.i = new ModelInstance(Mercury.m);
+		Mercury.i.transform.translate(Mercury.x, Mercury.y, Mercury.z);
 		//
-		venModel = modelBuilder.createSphere(venR, venR, venR, 40, 40, 
-											 new Material(ColorAttribute.createDiffuse(venC)),
-											 Usage.Position | Usage.Normal);
-		venus = new ModelInstance(venModel);
-		venus.transform.translate(venD, 0, venD);
-		//
-		earthModel = modelBuilder.createSphere(earR, earR, earR, 40, 40,
-											   new Material(ColorAttribute.createDiffuse(earC)), 
-											   Usage.Position | Usage.Normal);
-		earth = new ModelInstance(earthModel);
-		earth.transform.translate(earD, 0, earD);
-		//
-		marsModel = modelBuilder.createSphere(marR, marR, marR, 40, 40,
-											  new Material(ColorAttribute.createDiffuse(marC)), 
+		Venus = new Planet();
+		Venus.d = Sol.r + 1080f;
+		Venus.r = 12f;
+		Venus.c = new Color(Color.MAGENTA);
+		Venus.x = 0;
+		Venus.y = Venus.d;
+		Venus.z = 0;
+		Venus.div = 40;
+		Venus.m = modelBuilder.createSphere(Venus.r, Venus.r, Venus.r, Venus.div, Venus.div,
+											  new Material(ColorAttribute.createDiffuse(Venus.c)),
 											  Usage.Position | Usage.Normal);
-		mars = new ModelInstance(marsModel);
-		mars.transform.translate(marD, 0, marD);
+		Venus.i = new ModelInstance(Venus.m);
+		Venus.i.transform.translate(Venus.x, Venus.y, Venus.z);
 		//
-		jupModel = modelBuilder.createSphere(jupR, jupR, jupR, 40, 40,
-											 new Material(ColorAttribute.createDiffuse(jupC)), 
-											 Usage.Position | Usage.Normal);
-		jupiter = new ModelInstance(jupModel);
-		jupiter.transform.translate(jupD, 0, jupD);
+		Earth = new Planet();
+		Earth.d = Sol.r + 1500f;
+		Earth.r = 13f;
+		Earth.c = new Color(Color.CYAN);
+		Earth.x = 0;
+		Earth.y = 0;
+		Earth.z = Earth.d;
+		Earth.div = 40;
+		Earth.m = modelBuilder.createSphere(Earth.r, Earth.r, Earth.r, Earth.div, Earth.div,
+											new Material(ColorAttribute.createDiffuse(Earth.c)),
+											Usage.Position | Usage.Normal);
+		Earth.i = new ModelInstance(Earth.m);
+		Earth.i.transform.translate(Earth.x, Earth.y, Earth.z);
 		//
-		satModel = modelBuilder.createSphere(satR, satR, satR, 40, 40, 
-											 new Material(ColorAttribute.createDiffuse(satC)),
-											 Usage.Position | Usage.Normal);
-		saturn = new ModelInstance(satModel);
-		saturn.transform.translate(satD, 0, satD);
+		Mars = new Planet();
+		Mars.d = Sol.r + 2280f;
+		Mars.r = 7f;
+		Mars.c = new Color(Color.RED);
+		Mars.x = Mars.d;
+		Mars.y = 0;
+		Mars.z = Mars.d;
+		Mars.div = 40;
+		Mars.m = modelBuilder.createSphere(Mars.r, Mars.r, Mars.r, Mars.div, Mars.div,
+										   new Material(ColorAttribute.createDiffuse(Mars.c)),
+											Usage.Position | Usage.Normal);
+		Mars.i = new ModelInstance(Mars.m);
+		Mars.i.transform.translate(Mars.x, Mars.y, Mars.z);
 		//
-		uraModel = modelBuilder.createSphere(uraR, uraR, uraR, 40, 40,
-											 new Material(ColorAttribute.createDiffuse(uraC)), 
-											 Usage.Position | Usage.Normal);
-		uranus = new ModelInstance(uraModel);
-		uranus.transform.translate(uraD, 0, uraD);
+		Jupiter = new Planet();
+		Jupiter.d = Sol.r + 7780f;
+		Jupiter.r = 143f;
+		Jupiter.c = new Color(Color.GREEN.add(Color.BLUE));
+		Jupiter.x = Jupiter.d;
+		Jupiter.y = Jupiter.d;
+		Jupiter.z = 0;
+		Jupiter.div = 40;
+		Jupiter.m = modelBuilder.createSphere(Jupiter.r, Jupiter.r, Jupiter.r, Jupiter.div, Jupiter.div,
+											  new Material(ColorAttribute.createDiffuse(Jupiter.c)),
+										   Usage.Position | Usage.Normal);
+		Jupiter.i = new ModelInstance(Jupiter.m);
+		Jupiter.i.transform.translate(Jupiter.x, Jupiter.y, Jupiter.z);
 		//
-		nepModel = modelBuilder.createSphere(nepR, nepR, nepR, 40, 40,
-											 new Material(ColorAttribute.createDiffuse(nepC)), 
-											 Usage.Position | Usage.Normal);
-		neptune = new ModelInstance(nepModel);
-		neptune.transform.translate(nepD, 0, nepD);
+		Saturn = new Planet();
+		Saturn.d = Sol.r + 10800f;
+		Saturn.r = 120f;
+		Saturn.c = new Color(Color.YELLOW.add(Color.BLUE));
+		Saturn.x = Saturn.d;
+		Saturn.y = Saturn.d;
+		Saturn.z = Saturn.d;
+		Saturn.div = 40;
+		Saturn.m = modelBuilder.createSphere(Saturn.r, Saturn.r, Saturn.r, Saturn.div, Saturn.div,
+											 new Material(ColorAttribute.createDiffuse(Saturn.c)),
+											  Usage.Position | Usage.Normal);
+		Saturn.i = new ModelInstance(Saturn.m);
+		Saturn.i.transform.translate(Saturn.x, Saturn.y, Saturn.z);
 		//
-		pluModel = modelBuilder.createSphere(pluR, pluR, pluR, 40, 40,
-											 new Material(ColorAttribute.createDiffuse(pluC)), 
+		Uranus = new Planet();
+		Uranus.d = Sol.r + 15800f;
+		Uranus.r = 40f;
+		Uranus.c = new Color(Color.BLUE);
+		Uranus.x = 0;
+		Uranus.y = Uranus.d;
+		Uranus.z = Uranus.d;
+		Uranus.div = 40;
+		Uranus.m = modelBuilder.createSphere(Uranus.r, Uranus.r, Uranus.r, Uranus.div, Uranus.div,
+											 new Material(ColorAttribute.createDiffuse(Uranus.c)),
 											 Usage.Position | Usage.Normal);
-		pluto = new ModelInstance(pluModel);
-		pluto.transform.translate(pluD, 0, pluD);
-		//*** Keep together in create
+		Uranus.i = new ModelInstance(Uranus.m);
+		Uranus.i.transform.translate(Uranus.x, Uranus.y, Uranus.z);
+		//
+		Neptune = new Planet();
+		Neptune.d = Sol.r + 25800f;
+		Neptune.r = 70f;
+		Neptune.c = new Color(Color.GREEN);
+		Neptune.x = 0;
+		Neptune.y = 0;
+		Neptune.z = Neptune.d;
+		Neptune.div = 40;
+		Neptune.m = modelBuilder.createSphere(Neptune.r, Neptune.r, Neptune.r, Neptune.div, Neptune.div,
+											  new Material(ColorAttribute.createDiffuse(Neptune.c)),
+											 Usage.Position | Usage.Normal);
+		Neptune.i = new ModelInstance(Neptune.m);
+		Neptune.i.transform.translate(Neptune.x, Neptune.y, Neptune.z);
+		//
+		Pluto = new Planet();
+		Pluto.d = Sol.r + 78800f;
+		Pluto.r = 3f;
+		Pluto.c = new Color(Color.GRAY);
+		Pluto.x = Pluto.d;
+		Pluto.y = Pluto.d;
+		Pluto.z = Pluto.d;
+		Pluto.div = 40;
+		Pluto.m = modelBuilder.createSphere(Pluto.r, Pluto.r, Pluto.r, Pluto.div, Pluto.div,
+											new Material(ColorAttribute.createDiffuse(Pluto.c)),
+											 Usage.Position | Usage.Normal);
+		Pluto.i = new ModelInstance(Pluto.m);
+		Pluto.i.transform.translate(Pluto.x, Pluto.y, Pluto.z);
+		//
 
 		// Set control of camera to touch/pan/pinch
 		camController = new CameraInputController(cam);
 		Gdx.input.setInputProcessor(camController);
 	}
 
-	private void EnviroLights(){
+	public void EnviroLights(){
 		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.8f, 0.2f, 0.2f, 1f));
 		environment.clear();
 		environment.add(new DirectionalLight().set(dirLights, -1f, 0, 0));
@@ -202,9 +220,9 @@ public class solAdvanced implements ApplicationListener
 		environment.add(new DirectionalLight().set(dirLights, 0, 0, -1f));
 	}
 
-	private void CameraSet(){
+	public void CameraSet(){
 		cam = new PerspectiveCamera(50, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		cam.position.set(jupD + 500f, jupD + 500f, jupD + 500f);
+		cam.position.set(camPos, camPos, camPos);
 		cam.lookAt(0,0,0);
 		cam.near = 10f;
 		cam.far = 15000f;
@@ -219,9 +237,9 @@ public class solAdvanced implements ApplicationListener
 		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
-		DrawSol();
-		TimeElapse();
-		
+		DrawBodies();
+//		TimeElapse();
+
 		//   for (DirectionalLight light: environment.directionalLights){
 		//			light.set(0.8f, 0.8f, 0.8f, (float)Math.sin(dT), (float)Math.cos(dT), -0.2f);
 		//		}
@@ -236,32 +254,32 @@ public class solAdvanced implements ApplicationListener
 				dirLights = Color.GREEN;
 				EnviroLights();
 			}
-			environment.add(new DirectionalLight().set(Color.RED, touchPoint.x, touchPoint.y, -1f));
-		//	dirLights = Color.WHITE;
-		//	EnviroLights();
+//			if (pointInPlanet(Earth, touchPoint.x, touchPoint.y)) {
+//				dirLights = Color.RED;
+//				EnviroLights();
+//				}
+//				
+//			dirLights = Color.WHITE;
+//			EnviroLights();
 		}
-	//	dirLights = Color.WHITE;
-	//	EnviroLights();
-		//	earth.transform.rotate(1f, 1f, 0, dT);
-		//	sol.transform.translate(0, 0, dT);
 	}
 
-	public static boolean pointInPlanet (Planet r, float x, float y) {
-		return r.x <= x && r.x + r.w >= x && r.y <= y && r.y + r.h >= y;
+	public static boolean pointInPlanet (Planet p, float x, float y) {
+		return p.x <= x && p.x + p.r >= x && p.y <= y && p.y + p.r >= y;
 	}
-	public void DrawSol(){
+	
+	public void DrawBodies(){
 		modelBatch.begin(cam);
 		modelBatch.render(Sol.i, environment);
-		//	modelBatch.render(sol, environment);
-		modelBatch.render(mercury, environment);
-		modelBatch.render(venus, environment);
-		modelBatch.render(earth, environment);
-		modelBatch.render(mars, environment);
-		modelBatch.render(jupiter, environment);
-		modelBatch.render(saturn, environment);
-		modelBatch.render(neptune, environment);
-		modelBatch.render(uranus, environment);
-		modelBatch.render(pluto, environment);
+		modelBatch.render(Mercury.i, environment);
+		modelBatch.render(Venus.i, environment);
+		modelBatch.render(Earth.i, environment);
+		modelBatch.render(Mars.i, environment);
+		modelBatch.render(Jupiter.i, environment);
+		modelBatch.render(Saturn.i, environment);
+		modelBatch.render(Neptune.i, environment);
+		modelBatch.render(Uranus.i, environment);
+		modelBatch.render(Pluto.i, environment);
 		modelBatch.end();
 	}
 
@@ -269,17 +287,15 @@ public class solAdvanced implements ApplicationListener
 	public void dispose() {
 		modelBatch.dispose();
 		Sol.m.dispose();
-//		solModel.dispose();
-		earthModel.dispose();
-		mercModel.dispose();
-		venModel.dispose();
-		marsModel.dispose();
-		jupModel.dispose();
-		satModel.dispose();
-		uraModel.dispose();
-		nepModel.dispose();
-		pluModel.dispose();
-
+		Mercury.m.dispose();
+		Venus.m.dispose();
+		Earth.m.dispose();
+		Mars.m.dispose();
+		Jupiter.m.dispose();
+		Saturn.m.dispose();
+		Uranus.m.dispose();
+		Neptune.m.dispose();
+		Pluto.m.dispose();
 	}
 
 	@Override
